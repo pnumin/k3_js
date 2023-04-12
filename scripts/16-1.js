0093959e69ee1e5771613ca2555c50cda35cf2ef
pnumin <pnumin@pusan.ac.kr>
2023-04-12 11:26:25 +0900

    addEventListener

---
+++ scripts/16-1.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const bt1 = document.querySelector('#bt1') ;
+
+    bt1.addEventListener('click', () => {
+        event.preventDefault() ;
+
+        //랜덤수 생성
+        let n = Math.floor(Math.random() * 6) + 1 ;
+    
+        //사용자 입력 수 : 라디오의 체크값을 찾음
+        let user ;
+        const radios = document.querySelectorAll('input[type=radio]');
+        for(let item of radios) {
+            console.log(item);
+            if (item.checked) {
+                user = item.value ;
+                break;
+            }
+        }
+    
+        //랜덤수와 사용자입력수 같은지 검사
+        if (n === parseInt(user)) {
+            document.querySelector('article h1').textContent = '주사위게임 : 맞음(승)' ;
+        }
+        else {
+            document.querySelector('article h1').textContent = '주사위게임 : 틀림(패)' ;
+        }
+    
+        //결과출력
+        document.querySelector(".h2Class").innerHTML = `<img src="./images/${n}.png">`;
+    });
+
+});
--- scripts/16.js
+++ scripts/16.js
@@ -3,5 +3,26 @@ const show = () => {
 
     //랜덤수 생성
     let n = Math.floor(Math.random() * 6) + 1 ;
+
+    //사용자 입력 수 : 라디오의 체크값을 찾음
+    let user ;
+    const radios = document.querySelectorAll('input[type=radio]');
+    for(let item of radios) {
+        console.log(item);
+        if (item.checked) {
+            user = item.value ;
+            break;
+        }
+    }
+
+    //랜덤수와 사용자입력수 같은지 검사
+    if (n === parseInt(user)) {
+        document.querySelector('article h1').textContent = '주사위게임 : 맞음(승)' ;
+    }
+    else {
+        document.querySelector('article h1').textContent = '주사위게임 : 틀림(패)' ;
+    }
+
+    //결과출력
     document.querySelector(".h2Class").innerHTML = `<img src="./images/${n}.png">`;
 }
